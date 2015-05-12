b44c3e7d0272d2c53f9865b4c77a63bf6a85325d
TuanHoang277 <sotuanhoang@gmail.com>
2015-05-12 15:41:28 +0700

    Commit Background and tower

---
+++ Dota_Defene/Assets/Hoang_Asset/Scripts/BaseController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BaseController : MonoBehaviour {
+    public GameObject bullet;
+
+    public int hp;
+    public int damage;
+
+    public List<BaseController> targetList = new List<BaseController>();
+
+    
+    public void AddEnemyInRange(BaseController enemy)
+    {
+        targetList.Add(enemy);
+    }
+
+    public int RemoveEnemyInRange(BaseController enemy)
+    {
+        targetList.Remove(enemy);
+        return targetList.Count;
+    }
+
+    //public void SetEnemyInRange(bool EnemyInRange)
+    //{
+    //    targetList.SetBool(AnimatorParameters.EnemyInRange, EnemyInRange);
+    //}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public virtual void onAttack()
+    {
+    }
+
+    public virtual void Hit(int DMG)
+    {
+    }
+}
---
+++ Dota_Defene/Assets/Hoang_Asset/Scripts/TowerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TowerController : BaseController {
+
+    List<GameObject> listSolider;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
